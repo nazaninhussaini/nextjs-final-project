86d2bc65807b15871c109ac3d64e0880f457154e
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-09-11 20:55:12 +0430

    Nabar componend added

--- src/components/UiComponend/navbar.tsx
+++ src/components/UiComponend/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { onAuthStateChanged, User } from 'firebase/auth';
+import { onAuthStateChanged, signOut, User } from 'firebase/auth';
 import React, { useEffect, useState } from 'react'
 import { auth } from '../../../db/firebase.config';
 import { Button } from '../ui/button';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 function Navbar() {
-    const [user ,setUser] =useState<User|null>(null);
+    const [user ,setUser] =useState<User | null>(null);
+    const router = useRouter();
     useEffect(()=>{
         onAuthStateChanged(auth,(user)=>{
             if(user){
@@ -13,17 +16,25 @@ function Navbar() {
             }
         })
     },[])
+   async function handleLogout(){
+         await signOut(auth);
+         router.refresh();
+
+    }
   return (
     <div className='w-full border-b backdrop-blur-md fixed top-0 left-0 p-3 flex justify-between items-center '>
      <div>logo</div>
      <div>
-        {user ?(
+        {user ? (
             <div className='flex space-x-2'>
-                <div className='p-2 rounded-full bg-gray-600 text-stone-100 leading-0'>{user.displayName ?.slice(0,1).toUpperCase()}</div>
+                <Button onClick={handleLogout}></Button>
+                <div className='p-2 text-small px-2 rounded-full bg-gray-600 text-stone-100 leading-0'>{user.displayName ?.slice(0,1).toUpperCase()}</div>
                 <Button className="border">logout</Button>
                 </div>
         ):(
-            <button></button>
+            <Button>
+                <Link href="/login">Login</Link>
+            </Button>
         )}
      </div>
     </div>
